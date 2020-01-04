aa53931afa324707f7f379fab4fffe983a25d689
Vasily Vasilyev <vasar007@yandex.ru>
2020-01-04 16:28:44 +0300

    Fix samples and turn off warning for XML doc

--- README.md
+++ README.md
@@ -30,7 +30,7 @@ private static async Task Examples()
 
     // Get full list of SteamApps.
     SteamAppBriefInfoList steamAppList = await steamApiClient.GetAppListAsync();
-    Console.WriteLine($"Got {steamAppList.Apps.Length} items.");
+    Console.WriteLine($"Got {steamAppList.Apps.Length.ToString()} items.");
 
     // Get details for SteamApp with ID 292030 (The Witcher 3: Wild Hunt).
     SteamApp steamApp1 = await steamApiClient.GetSteamAppAsync(292030);
@@ -46,23 +46,23 @@ private static async Task Examples()
 
     // Get details for Package with same ID for region JP.
     PackageInfo package2 = await steamApiClient.GetPackageInfoAsync(68179, CountryCode.Japan);
-    Console.WriteLine($"Got response for {package1.Name}.");
+    Console.WriteLine($"Got response for {package2.Name}.");
 
     // Get a list of featured games.
     FeaturedApps featured1 = await steamApiClient.GetFeaturedAppsAsync();
-    Console.WriteLine($"Got {featured1.FeaturedWin.Length} items for Winodws.");
+    Console.WriteLine($"Got {featured1.FeaturedWin.Length.ToString()} items for Windows.");
 
     // Get a list of featured games for region DE.
     FeaturedApps featured2 = await steamApiClient.GetFeaturedAppsAsync(CountryCode.Germany);
-    Console.WriteLine($"Got {featured2.FeaturedWin.Length} items for Winodws.");
+    Console.WriteLine($"Got {featured2.FeaturedWin.Length.ToString()} items for Windows.");
 
     // Get a list of featured games grouped by category.
     FeaturedCategories featuredCategories1 = await steamApiClient.GetFeaturedCategoriesAsync();
-    Console.WriteLine($"Got {featuredCategories1.TopSellers.Items.Length} top sellers items.");
+    Console.WriteLine($"Got {featuredCategories1.TopSellers.Items.Length.ToString()} top sellers items.");
 
     // Get a list of featured games grouped by category for region US.
     FeaturedCategories featuredCategories2 = await steamApiClient.GetFeaturedCategoriesAsync(CountryCode.USA);
-    Console.WriteLine($"Got {featuredCategories2.TopSellers.Items.Length} top sellers items.");
+    Console.WriteLine($"Got {featuredCategories2.TopSellers.Items.Length.ToString()} top sellers items.");
 }
 ```
 
--- SteamWebApiLibConsoleApp/Program.cs
+++ SteamWebApiLibConsoleApp/Program.cs
@@ -11,17 +11,30 @@ namespace SteamWebApiLibConsoleApp
 {
     internal static class Program
     {
-        private static async Task Main()
+        private static async Task<int> Main()
         {
             try
             {
+                Console.WriteLine("Console application started.");
+                
                 await Examples();
+
+                return 0;
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex);
+                string exceptionMessage = $"Exception occurred in {nameof(Main)} method. " +
+                                          $"{Environment.NewLine}{ex.ToString()}";
+                Console.WriteLine(exceptionMessage);
+
+                return -1;
             }
-            Console.ReadLine();
+            finally
+            {
+                Console.WriteLine("Console application stopped.");
+                Console.WriteLine("Press any key to close this window...");
+                Console.ReadKey();
+            }
         }
 
         private static async Task Examples()
@@ -30,7 +43,7 @@ namespace SteamWebApiLibConsoleApp
 
             // Get full list of SteamApps.
             SteamAppBriefInfoList steamAppList = await steamApiClient.GetAppListAsync();
-            Console.WriteLine($"Got {steamAppList.Apps.Length} items.");
+            Console.WriteLine($"Got {steamAppList.Apps.Length.ToString()} items.");
 
             // Get details for SteamApp with ID 292030 (The Witcher 3: Wild Hunt).
             SteamApp steamApp1 = await steamApiClient.GetSteamAppAsync(292030);
@@ -46,23 +59,23 @@ namespace SteamWebApiLibConsoleApp
 
             // Get details for Package with same ID for region JP.
             PackageInfo package2 = await steamApiClient.GetPackageInfoAsync(68179, CountryCode.Japan);
-            Console.WriteLine($"Got response for {package1.Name}.");
+            Console.WriteLine($"Got response for {package2.Name}.");
 
             // Get a list of featured games.
             FeaturedApps featured1 = await steamApiClient.GetFeaturedAppsAsync();
-            Console.WriteLine($"Got {featured1.FeaturedWin.Length} items for Winodws.");
+            Console.WriteLine($"Got {featured1.FeaturedWin.Length.ToString()} items for Windows.");
 
             // Get a list of featured games for region DE.
             FeaturedApps featured2 = await steamApiClient.GetFeaturedAppsAsync(CountryCode.Germany);
-            Console.WriteLine($"Got {featured2.FeaturedWin.Length} items for Winodws.");
+            Console.WriteLine($"Got {featured2.FeaturedWin.Length.ToString()} items for Windows.");
 
             // Get a list of featured games grouped by category.
             FeaturedCategories featuredCategories1 = await steamApiClient.GetFeaturedCategoriesAsync();
-            Console.WriteLine($"Got {featuredCategories1.TopSellers.Items.Length} top sellers items.");
+            Console.WriteLine($"Got {featuredCategories1.TopSellers.Items.Length.ToString()} top sellers items.");
 
             // Get a list of featured games grouped by category for region US.
             FeaturedCategories featuredCategories2 = await steamApiClient.GetFeaturedCategoriesAsync(CountryCode.USA);
-            Console.WriteLine($"Got {featuredCategories2.TopSellers.Items.Length} top sellers items.");
+            Console.WriteLine($"Got {featuredCategories2.TopSellers.Items.Length.ToString()} top sellers items.");
         }
     }
 }
